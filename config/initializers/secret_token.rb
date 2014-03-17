9c344f7eea064217747859549c579f4a7a2292ea
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-03-17 17:06:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RatingApp::Application.config.secret_key_base = '1bfeaa8c2d69cd96512f256b55b5dca8c03206f364d6eb0114aa8e2ede72b8282d055b13e474723fd3794f7f0d1c30089e536faee1af1ddb094752896ff7adb7'
