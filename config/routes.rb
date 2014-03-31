f86cda8c01118d969b7f1ecb82416ae3289554cf
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-03-31 15:22:23 -0400

    Added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RatingApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
   resources :reviews
 
   resources :users
