9228f59eef7d6ce8661120986ed16fb081fe2739
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-04-07 11:03:52 -0400

    Finished layout and routes

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
-gem 'rails', '4.0.3'
+gem 'rails', '4.0.4'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 RatingApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :reviews
 
   resources :users
