265f9eed413f69d5230ee125598cf99d1f3a618e
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-04-08 06:25:27 -0400

    Made a basic user model with secure password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RatingApp::Application.routes.draw do
+  get "users/new"
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
