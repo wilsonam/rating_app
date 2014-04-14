85a8f42bbc63aac6cfa7049c656b464e6828420f
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-04-14 11:24:13 -0400

    Finished sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
 
 	def show
-    	@user = User.find(params[:id])
-  	end
+   @user = User.find(params[:id])
+  end
 
-  	def new
-  		@user = User.new
-  	end
+  def new
+   @user = User.new
+  end
 
-  	def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Review App!"
       redirect_to @user
     else
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+       :password_confirmation)
     end
 end
