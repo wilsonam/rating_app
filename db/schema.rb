5d352697b2b4d9b42e1a203540d67f69c0ef5e88
Aaron Wilson <aaronwilson@Aarons-MacBook-Air.local>
2014-04-15 15:05:28 -0400

    Finished user edit, update, index, and destroy as well as admin rights

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414142446) do
+ActiveRecord::Schema.define(version: 20140415162118) do
 
   create_table "reviews", force: true do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140414142446) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
