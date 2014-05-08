13883fe3f2c40253989975c32418362a824e3ba9
Aaron Wilson <aaronwilson@dhcp-137-112-239-172.wlan.rose-hulman.edu>
2014-05-08 08:21:25 -0400

    finished implementing microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 	uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
 	has_many :reviews
+	has_many :microposts, dependent: :destroy
 	has_secure_password
 
 	def User.new_remember_token
@@ -16,6 +17,11 @@ class User < ActiveRecord::Base
 
 	def User.hash(token)
 		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
 	end
 
 	private
